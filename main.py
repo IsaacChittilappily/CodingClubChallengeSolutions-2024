c6d12cbeb97c4353037341f77da055b9601776ea
Isaac Chittilappily <chittilappilyisaac@gmail.com>
2024-06-04 20:30:57 +0000

    Changed folder names to uncapitalised (cuz who even likes capitals??)

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ with open('config.txt','r') as f:
   # replace the number in config.txt with the week number
   folder = f.read().strip()
 
-folderPath = './Week' + folder
+folderPath = './week' + folder
 
 if folderPath not in sys.path:
   sys.path.append(folderPath)
