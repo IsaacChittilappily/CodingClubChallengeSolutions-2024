b56787d9c9963ece16a60b4faa0f814f15036c90
Isaac Chittilappily <chittilappilyisaac@gmail.com>
2024-06-17 10:31:51 +0000

    Added solution to week 5 (sorry that its late!)

--- week5/solution5.py
+++ week5/solution5.py
@@ -1,1 +1,16 @@
-# will be released on Tuesday evening
+def singleNum(nums: list[int]) -> int:
+
+  result = 0
+  for num in nums:
+    result ^= num
+
+  return result
+
+def main():
+
+  nums = [1,2,3,3,4,2,1]
+  print('Nums = ', nums)
+  print('Single number =', singleNum(nums))
+
+if __name__ == '__main__':
+  main()
