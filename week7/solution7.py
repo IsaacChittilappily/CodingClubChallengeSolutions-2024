619d4a4f1827d868d2a699c9c831d8a19a5576d0
Isaac Chittilappily <chittilappilyisaac@gmail.com>
2024-06-30 19:41:48 +0000

    Added better conventions in the functions

--- week7/solution7.py
+++ week7/solution7.py
@@ -1,10 +1,10 @@
-def evenSum(nums):
+def evenSum(nums: list[int]) -> int:
   return sum([num for num in nums if num % 2 == 0])
 
-def addOther(nums, add):
+def addOther(nums: list[int], add: int) -> list[int]:
   return [num + add for index, num in enumerate(nums) if index % 2 == 0]
 
-def shiftList(nums, shift):
+def shiftList(nums: list[int], shift: int) -> list[int]:
   return nums[-shift:] + nums[:-shift]
 
 
