670f483954cf069586992c860e99f104f6809115
Isaac Chittilappily <chittilappilyisaac@gmail.com>
2024-07-10 10:44:42 +0000

    Added solution to problem 9

--- week9/solution9.py
+++ week9/solution9.py
@@ -1,3 +1,20 @@
 def romanToInt(roman: str) -> int:
-  ...
-  # code goes here
+  symbols = {
+      'I': 1,
+      'V': 5,
+      'X': 10,
+      'L': 50,
+      'C': 100,
+      'D': 500,
+      'M': 1000
+  }
+
+  ans = 0
+
+  for i in range(len(roman)):
+      if i < len(roman) - 1 and symbols[roman[i]] < symbols[roman[i+1]]:
+          ans -= symbols[roman[i]]
+      else:
+          ans += symbols[roman[i]]
+
+  return ans
